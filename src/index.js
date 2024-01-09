f2c4048447e638b845de98aaa6c38e4d1e80bcbc
mia-chou <mia.fy.chou@hotmail.com>
2024-01-09 12:03:39 +0000

    Add search the function using API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
-function handleSearchSubmit(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input");
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+  
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
   }
   
-  let searchFormElement = document.querySelector("#search-form");
-  searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+
+  function searchCity(city) {
+    let apiKey = "99e882a3db10efo5bb4b346d32e0a6t0";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearchSubmit(event) {
+event.preventDefault();
+let searchInput = document.querySelector("#search-form-input");
+
+searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
