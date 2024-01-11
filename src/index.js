9a929d96e258a0b6a2cea47db921db9477460785
mia-chou <mia.fy.chou@hotmail.com>
2024-01-11 03:33:55 +0000

    Inject HTML from JavaScript

--- src/index.js
+++ src/index.js
@@ -46,14 +46,35 @@ let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${api
 axios.get(apiUrl).then(refreshWeather);
 }
 
+
+
+
 function handleSearchSubmit(event) {
 event.preventDefault();
 let searchInput = document.querySelector("#search-form-input");
 
+
+
+
 searchCity(searchInput.value);
 }
+
+
+
+
+
+
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
+let forecast = document.querySelector("#forecast");
+forecastElement.innerHTML = forecastHtml;
+
+
+
 searchCity("Leeds");
+displayForecast();
+
+
+
